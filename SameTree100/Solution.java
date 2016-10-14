9760f915b74e9bd3e8b782d614bb890389f9a92f
ppulse <ustbtobuaa_zxm@outlook.com>
2016-10-15 00:54:48 +0800

    add solution of problem 100: Same Tree

---
+++ SameTree100/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        else if (p == null || q == null)
+            return false;
+        
+        if (p.val == q.val) {
+            boolean isLeftSameTree = isSameTree(p.left, q.left);
+            if (isLeftSameTree) {
+                boolean isRightSameTree = isSameTree(p.right, q.right);
+                if (isRightSameTree) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+}
