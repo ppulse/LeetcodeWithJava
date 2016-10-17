92c2018dd1ffc98a8edb4d7620f09977e8f5f1bf
ppulse <ustbtobuaa_zxm@outlook.com>
2016-10-18 00:27:18 +0800

    refactor problem 13: Roman To Integer

--- RomanToInteger13/Solution.java
+++ RomanToInteger13/Solution.java
@@ -1,30 +1,41 @@
-import java.util.Map;
-import java.util.HashMap;
 
 public class Solution {
     public int romanToInt(String s) {
         if (s == null)
             return 0;
-        
-        Map<Character, Integer> map = new HashMap<Character, Integer>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
 
         int currentValue = 0;
         int result = 0;
         
         for (int i = s.length() - 1; i >= 0; i--) {
             char currentChar = s.charAt(i);
-            if (!map.containsKey(currentChar))
-                return 0;
-            
+
             int preValue = currentValue;
-            currentValue = map.get(currentChar);
+            switch (currentChar) {
+                case 'I': 
+                    currentValue = 1;
+                    break;
+                case 'V': 
+                    currentValue = 5;
+                    break;
+                case 'X':
+                    currentValue = 10;
+                    break;
+                case 'L': 
+                    currentValue = 50;
+                    break;
+                case 'C': 
+                    currentValue = 100;
+                    break;
+                case 'D': 
+                    currentValue = 500;
+                    break;
+                case 'M': 
+                    currentValue = 1000;
+                    break;
+                default: 
+                    return 0;
+            }
             
             if (preValue > currentValue) {
                 result -= currentValue;
