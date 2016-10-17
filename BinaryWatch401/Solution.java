65210167a2b9fce243943c20b54ba9b53ca7e25b
ppulse <ustbtobuaa_zxm@outlook.com>
2016-10-17 21:38:20 +0800

    refactor problem 401: Binary Watch

--- BinaryWatch401/Solution.java
+++ BinaryWatch401/Solution.java
@@ -4,32 +4,13 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Solution {
+    private Map<Integer, List<Integer>> hourContainer = new HashMap<Integer, List<Integer>>();
+    private Map<Integer, List<Integer>> minuteContainer = new HashMap<Integer, List<Integer>>();
+    
     public List<String> readBinaryWatch(int num) {
-        Map<Integer, List<Integer>> hourContainer = new HashMap<Integer, List<Integer>>();
-        Map<Integer, List<Integer>> minuteContainer = new HashMap<Integer, List<Integer>>();
-        
-        for (int i = 0; i < 12; i++) {
-            if (!hourContainer.containsKey(numOfBitOne(i))) {
-                List<Integer> list = new ArrayList<Integer>();
-                list.add(i);
-                hourContainer.put(numOfBitOne(i), list);
-            } else {
-                hourContainer.get(numOfBitOne(i)).add(i);
-            }
-        }
-        
-        for (int i = 0; i < 60; i++) {
-            if (!minuteContainer.containsKey(numOfBitOne(i))) {
-                List<Integer> list = new ArrayList<Integer>();
-                list.add(i);
-                minuteContainer.put(numOfBitOne(i), list);
-            } else {
-                minuteContainer.get(numOfBitOne(i)).add(i);
-            }
-        }
+        cacheNumsOfBitOne();
         
         List<String> result = new ArrayList<String>();
-   
         for (int i = 0; i < 4 && i <= num; i++) {
             List<Integer> hourList = hourContainer.get(i);
             List<Integer> minuteList = minuteContainer.get(num - i);
@@ -37,22 +18,39 @@ public class Solution {
             if (minuteList != null) {
                 for (int hour : hourList) {
                     for (int minute : minuteList) {
-                        StringBuilder sb = new StringBuilder();
-                        sb.append(hour);
-                        sb.append(":");
-                        if (minute < 10) {
-                            sb.append("0");
-                        }
-                        sb.append(minute);
-                        
-                        result.add(sb.toString());
+                        result.add(hourMinuteToString(hour, minute));
                     }
                 }
             }
         }
         
-        
         return result;
+    }
+    
+    private void cacheNumsOfBitOne() {
+        cacheNumsOfBitOneHour();
+        
+        cacheNumsOfBitOneMinute();
+    }
+    
+    private void cacheNumsOfBitOneHour() {
+        cacheNumsOfBitOne(hourContainer, 12);
+    }
+    
+    private void cacheNumsOfBitOneMinute() {
+        cacheNumsOfBitOne(minuteContainer, 60);
+    }
+    
+    private void cacheNumsOfBitOne(Map<Integer, List<Integer>> container, int loopTimes) {
+        for (int i = 0; i < loopTimes; i++) {
+            if (!container.containsKey(numOfBitOne(i))) {
+                List<Integer> list = new ArrayList<Integer>();
+                list.add(i);
+                container.put(numOfBitOne(i), list);
+            } else {
+                container.get(numOfBitOne(i)).add(i);
+            }
+        }
     }
     
     private int numOfBitOne(int num) {
@@ -65,5 +63,16 @@ public class Solution {
         }
         
         return cnt;
+    }
+    
+    private String hourMinuteToString(int hour, int minute) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(hour);
+        sb.append(":");
+        if (minute < 10) {
+            sb.append("0");
+        }
+        sb.append(minute);
+        return sb.toString();
     }
 }
