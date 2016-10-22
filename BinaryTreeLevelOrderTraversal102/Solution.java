fbc31d29a7e462927b61c8fdca3c455ee3820638
ppulse <ustbtobuaa_zxm@outlook.com>
2016-10-22 23:25:10 +0800

    other method to solve problem 102: Binary Tree Level Order Traveral

--- BinaryTreeLevelOrderTraversal102/Solution.java
+++ BinaryTreeLevelOrderTraversal102/Solution.java
@@ -8,29 +8,114 @@
  * }
  */
 public class Solution {
+    // public List<List<Integer>> levelOrder(TreeNode root) {
+    //     List<List<Integer>> result = new ArrayList<List<Integer>>();
+        
+    //     levelOrder(root, result, 1);
+        
+    //     return result;
+    // }
+    
+    // private void levelOrder(TreeNode root, List<List<Integer>> result, int depth) {
+    //     if (root == null)
+    //         return;
+        
+    //     List<Integer> levelContainer = null;
+    //     if (result.size() < depth) {
+    //         levelContainer = new ArrayList<Integer>();
+    //         levelContainer.add(root.val);
+    //         result.add(levelContainer);
+    //     } else {
+    //         levelContainer = result.get(depth - 1);
+    //         levelContainer.add(root.val);
+    //     }
+        
+    //     levelOrder(root.left, result, depth + 1);
+    //     levelOrder(root.right, result, depth + 1);
+    // }
+    // 
+
+    // public List<List<Integer>> levelOrder(TreeNode root) {
+    //     List<List<Integer>> result = new ArrayList<List<Integer>>();
+        
+    //     if (root == null)
+    //         return result;
+        
+    //     Queue<TreeNode> currentLevel = new LinkedList<TreeNode>();
+    //     currentLevel.offer(root);
+        
+    //     while (!currentLevel.isEmpty()) {
+    //         int size = currentLevel.size();
+    //         List<Integer> currList = new ArrayList<Integer>();
+            
+    //         for (int i = 0; i < size; i++) {
+    //             TreeNode node = currentLevel.poll();
+    //             currList.add(node.val);
+                
+    //             if (node.left != null) {
+    //                 currentLevel.offer(node.left);    
+    //             }
+            
+    //             if (node.right != null) {
+    //                 currentLevel.offer(node.right);
+    //             }
+                
+    //         }
+            
+    //         result.add(currList);
+    //     }
+        
+        
+    //     return result;
+    // }
+
+
+    private class TreeNodeWithDepth {
+        int depth;
+        TreeNode node;
+        
+        public TreeNodeWithDepth(TreeNode node, int depth) {
+            this.node = node;
+            this.depth = depth;
+        }
+    }
+    
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         
-        levelOrder(root, result, 1);
-        
-        return result;
-    }
-    
-    private void levelOrder(TreeNode root, List<List<Integer>> result, int depth) {
         if (root == null)
-            return;
+            return result;
         
-        List<Integer> levelContainer = null;
-        if (result.size() < depth) {
-            levelContainer = new ArrayList<Integer>();
-            levelContainer.add(root.val);
-            result.add(levelContainer);
-        } else {
-            levelContainer = result.get(depth - 1);
-            levelContainer.add(root.val);
+        Queue<TreeNodeWithDepth> queue = new LinkedList<TreeNodeWithDepth>();
+        
+        queue.offer(new TreeNodeWithDepth(root, 1));
+        
+        while (!queue.isEmpty()) {
+            TreeNodeWithDepth node = queue.poll();
+                        
+            TreeNode treeNode = node.node;
+            int depth = node.depth;
+            
+            List<Integer> currList;
+            if (result.size() < node.depth) {
+                currList = new ArrayList<Integer>();
+                currList.add(treeNode.val);
+                result.add(currList);
+            } else {
+                currList = result.get(depth - 1);
+                currList.add(treeNode.val);
+            }
+            
+            if (treeNode.left != null) {
+                queue.offer(new TreeNodeWithDepth(treeNode.left, depth + 1));
+            }
+            
+            if (treeNode.right != null) {
+                queue.offer(new TreeNodeWithDepth(treeNode.right, depth + 1));    
+            }
         }
         
-        levelOrder(root.left, result, depth + 1);
-        levelOrder(root.right, result, depth + 1);
+        
+        return result;
     }
 }
