792f4193ec28ff079f7fe01f76edebcb5162048b
ppulse <ustbtobuaa_zxm@outlook.com>
2016-11-09 23:50:21 +0800

    add solution of problem 278: first bad version

---
+++ FirstBadVersion278/Solution.java
@@ -0,0 +1,20 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        long right = n;
+        
+        while (left <= right) {
+            int mid = (int)((left + right) >> 1);
+            if (isBadVersion(mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+}
