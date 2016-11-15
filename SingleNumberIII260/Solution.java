70d7486d8d804c9e834621e1628b555ab9d244dc
ppulse <ustbtobuaa_zxm@outlook.com>
2016-11-15 23:35:15 +0800

    refactoring problem 260

--- SingleNumberIII260/Solution.java
+++ SingleNumberIII260/Solution.java
@@ -9,36 +9,14 @@ public class Solution {
             xorResult ^= nums[i];
         
         int partitionNum = xorResult & (xorResult ^ (xorResult - 1));
-        List<List<Integer>> twoParts = partition(nums, partitionNum);
-        result[0] = singleNumberOneValue(twoParts.get(0));
-        result[1] = singleNumberOneValue(twoParts.get(1));
-        
-        return result;
-    }
-    
-    private List<List<Integer>> partition(int[] nums, int partitionNum) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        List<Integer> partOne = new ArrayList<Integer>();
-        List<Integer> partTwo = new ArrayList<Integer>();
         
         for (int i = 0; i < nums.length; i++) {
             if ((nums[i] & partitionNum) == 0)
-                partOne.add(nums[i]);
+                result[0] ^= nums[i];
             else
-                partTwo.add(nums[i]);
+                result[1] ^= nums[i];
         }
         
-        result.add(partOne);
-        result.add(partTwo);
-        
-        return result;
-    }
-    
-    private int singleNumberOneValue(List<Integer> nums) {
-        int result = 0;
-        Iterator<Integer> iter = nums.iterator();
-        while (iter.hasNext())
-            result ^= iter.next();
         return result;
     }
 }
