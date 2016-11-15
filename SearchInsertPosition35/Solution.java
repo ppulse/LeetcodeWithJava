6dd827cecea8da5fa664b5780417747e445af8d3
ppulse <ustbtobuaa_zxm@outlook.com>
2016-11-15 22:52:20 +0800

    add solution of problem 35: search insert position

---
+++ SearchInsertPosition35/Solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (nums[mid] > target)
+                right = mid - 1;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                return mid;
+        }
+        
+        return left;
+    }
+}
