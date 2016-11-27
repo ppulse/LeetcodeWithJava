6c02f06ee53c4484e883120412b490690a9a2960
ppulse <ustbtobuaa_zxm@outlook.com>
2016-11-27 21:37:23 +0800

    refactor problem 120: merge last list to common process

--- Triangle120/Solution.java
+++ Triangle120/Solution.java
@@ -1,19 +1,40 @@
 public class Solution {
+    // public int minimumTotal(List<List<Integer>> triangle) {
+    //     if (triangle == null || triangle.size() == 0)
+    //         return 0;
+        
+    //     int size = triangle.size();
+    //     int[] sum = new int[size];
+        
+    //     List<Integer> lastList = triangle.get(size - 1);
+        
+    //     int idx = 0;
+    //     for (int val : lastList) {
+    //         sum[idx++] = val;
+    //     }
+        
+    //     for (int i = size - 2; i >= 0; i--) {
+    //         List<Integer> ls = triangle.get(i);
+    //         int lsSize = ls.size();
+    //         for (int j = 0; j < lsSize; j++) {
+    //             int minValue = sum[j] < sum[j+1] ? sum[j] : sum[j+1];
+    //             sum[j] = ls.get(j) + minValue; 
+    //         }
+    //     }
+        
+    //     return sum[0];
+    // }
+
     public int minimumTotal(List<List<Integer>> triangle) {
         if (triangle == null || triangle.size() == 0)
             return 0;
         
         int size = triangle.size();
-        int[] sum = new int[size];
+        int[] sum = new int[size+1];
         
         List<Integer> lastList = triangle.get(size - 1);
         
-        int idx = 0;
-        for (int val : lastList) {
-            sum[idx++] = val;
-        }
-        
-        for (int i = size - 2; i >= 0; i--) {
+        for (int i = size - 1; i >= 0; i--) {
             List<Integer> ls = triangle.get(i);
             int lsSize = ls.size();
             for (int j = 0; j < lsSize; j++) {
