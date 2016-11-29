fcfb6d90c9742c04db07cdbb49dc899554743666
ppulse <ustbtobuaa_zxm@outlook.com>
2016-11-29 23:57:45 +0800

    refactor problem 144: use iterator

--- BinaryTreePreorderTraversal144/Solution.java
+++ BinaryTreePreorderTraversal144/Solution.java
@@ -8,21 +8,46 @@
  * }
  */
 public class Solution {
+    // public List<Integer> preorderTraversal(TreeNode root) {
+    //     List<Integer> result = new ArrayList<Integer>();
+        
+    //     if (root != null)
+    //         preorderTraversal(root, result);
+            
+    //     return result;
+    // }
+    
+    // private void preorderTraversal(TreeNode node, List<Integer> result) {
+    //     result.add(node.val);
+    //     if (node.left != null)
+    //         preorderTraversal(node.left, result);
+        
+    //     if (node.right != null)
+    //         preorderTraversal(node.right, result);
+    // }
+    // 
+    
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<Integer>();
         
-        if (root != null)
-            preorderTraversal(root, result);
-            
-        return result;
-    }
-    
-    private void preorderTraversal(TreeNode node, List<Integer> result) {
-        result.add(node.val);
-        if (node.left != null)
-            preorderTraversal(node.left, result);
+        Stack<TreeNode> stack = new Stack<TreeNode>();
         
-        if (node.right != null)
-            preorderTraversal(node.right, result);
+        if (root != null) {
+            stack.push(root);
+            while (!stack.empty()) {
+                TreeNode node = stack.pop();
+                result.add(node.val);
+                
+                if (node.right != null) {
+                    stack.push(node.right);
+                }
+                
+                if (node.left != null) {
+                    stack.push(node.left);
+                }
+            }
+        }
+        
+        return result;
     }
 }
