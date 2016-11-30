0b7f8ffe5bba9ef53821e6d67990107acb07f38a
ppulse <ustbtobuaa_zxm@outlook.com>
2016-11-30 22:13:23 +0800

    refactor problem 319: (int)Math.sqrt(n) is the result

--- BulbSwitcher319/Solution.java
+++ BulbSwitcher319/Solution.java
@@ -1,16 +1,26 @@
 public class Solution {
+    // public int bulbSwitch(int n) {
+    //     if (n < 0)
+    //         return Integer.MIN_VALUE;
+            
+    //     int cnt = 0;
+        
+    //     int value = 1;
+    //     while (value * value <= n) {
+    //         cnt++;
+    //         value++;
+    //     }
+        
+    //     return cnt;
+    // }
+    // 
+
     public int bulbSwitch(int n) {
         if (n < 0)
             return Integer.MIN_VALUE;
-            
-        int cnt = 0;
+
+        int result = (int)Math.sqrt(n);
         
-        int value = 1;
-        while (value * value <= n) {
-            cnt++;
-            value++;
-        }
-        
-        return cnt;
+        return result;
     }
 }
